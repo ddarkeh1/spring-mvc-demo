b3c20f4eb0345aa90477cc73bc1d3c370665043c
Steven Mitchell <ddarkeh1@gmail.com>
2018-04-04 15:41:29 +0100

    Included a one-to-one example with Cascading data

---
+++ src/com/ddarkeh/hibernate/demo/CreateDemo.java
@@ -0,0 +1,51 @@
+package com.ddarkeh.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ddarkeh.hibernate.demo.entity.Instructor;
+import com.ddarkeh.hibernate.demo.entity.InstructorDetail;
+import com.ddarkeh.hibernate.demo.entity.Student;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		// create a session
+		
+		Session session = factory.getCurrentSession();
+		
+		try {			
+
+			Instructor tempInstructor =
+					new Instructor("Charlotte", "Webb", "Charl.Webb@hotmail.com");
+			
+			InstructorDetail tempInstructorDetail =
+					new InstructorDetail("http://www.youtube.com/web", "Footi!!");
+			
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			session.save(tempInstructor);
+			
+			// commit the db transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+			
+		} finally {
+			factory.close();
+		}
+	}
+
+}
